3adfc76b0ae2d55b5f7a430669df2b6f4fa7f528
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-02-18 14:13:26 -0500

    reboot suddenly

--- tilde.mk
+++ tilde.mk
@@ -1,15 +1,13 @@
 ## This file is the Makefile for ~dushoff, curated in linux_config
 ## It should be a link, not a copy. Use `make relink` (there) to fix
 
-PUSH = perl -wf $(filter %.pl, $^) $(filter-out %.pl, $^) > $@
-
 -include target.mk
 
 screens: 
 	git clone https://github.com/dushoff/screens.git
 
+start screenstart setup buildscreen:
+	cd screens && make $@
+
 update:
 	(cd screens/linux_config && make pull) || (cd screens/*/linux_config && make pull)
-
-start:
-	cd screens && make start
