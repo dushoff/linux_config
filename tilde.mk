2765c6c45a5972407c4d5a61d926421b540ab664
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-05-18 23:09:06 -0400

    Simplify tilde.mk

--- tilde.mk
+++ tilde.mk
@@ -1,10 +1,7 @@
 ## This file is the Makefile for ~dushoff, curated in linux_config
-## It should be a link, not a copy. Use `make relink` (there) to fix
 
--include target.mk
-
-screens: 
-	git clone https://github.com/dushoff/screens.git
-
-start screenstart setup buildscreen:
+start setup makescreen:
 	cd screens && make $@
+
+relink:
+	cd */*/linux_config && $(MAKE) relink
