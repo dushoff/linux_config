09d0e91cacc36924cc6125071c4b2e0a4e15f237
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-06-24 17:20:25 -0400

    Still cleaning

---
+++ bin/quote.pl
@@ -0,0 +1,27 @@
+#! /usr/bin/perl
+
+use strict;
+use 5.10.0;
+
+while(<>){
+	s/\bdash\b/–/gi;
+	s/Pete/epiEstim/g;
+	s/Mac pan/MacPan/g;
+	s/champagne/MacPan/g;
+	s/ oh,/,/g;
+	s/\bthis up\b/the supp/g;
+	s/a notated/annotated/g;
+	s/Marcia/Reshma/g;
+	s/Manuel/manual/g;
+	s/Repose/repos/g;
+	s/Arnold/R0/g;
+	s/\s*semi:/;/g;
+	s/Fungo/Chyun/g;
+	s/Macy's/SACEMA/g;
+	s/afro/AFRO/g;
+	s/icebound/ICI3D/gi;
+	s/Georgian/Bayesian/g;
+	s/freakish/frequentist/g;
+	s/Massif/MSAF/gi;
+	print;
+}
