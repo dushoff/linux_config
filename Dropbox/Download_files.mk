27b0014c91f430bd63e1fd08732be6034ba69b9f
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-07-10 16:35:46 -0400

    Leaving office

--- Dropbox/Download_files.mk
+++ Dropbox/Download_files.mk
@@ -2,6 +2,8 @@
 ## A nogit Makefile for Download files
 ## When you archive a Downloads folder:
 ## copy the old-folder Makefile to the new folder
+## This is revision controlled for some reason; push and pull to 
+## ~/screens/tech/linux_config/Dropbox/Download_files.mk
 
 2407.newDown:
 %.newDown:
@@ -11,11 +13,13 @@
 	cd ~/Downloads && $(MAKE) makestuff && $(MAKE) target.mk
 
 ## 2407.newcomputer:
+## This should keep tmpDownloads if it's a real directory
+## and ditch it if it's a symbolic link to the thing you're deprecating
 %.newcomputer:
 	- mv ~/Downloads ~/tmpDownloads
 	cd ~ && ln -s ~/Dropbox/Download_files/$* Downloads
 	- mv ~/tmpDownloads ~/Downloads/
-
+	$(RM) ~/tmpDownloads
 
 ######################################################################
 
