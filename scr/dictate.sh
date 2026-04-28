d7301d51b0c12d1905b7596956e78245db1d9707
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-04-28 18:10:16 -0400

    332

--- scr/dictate.sh
+++ scr/dictate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-TMPFILE=$(mktemp /tmp/dictate-XXXXXX.wav)
-sox -d -r 16000 -c 1 "$TMPFILE"
-TEXT=$(~/screens/tech/linux_setup/whisper.cpp/build/bin/whisper-cli \
-	-m ~/screens/tech/linux_setup/whisper.cpp/models/ggml-small.en.bin \
-	-f "$TMPFILE" --no-timestamps 2>/dev/null)
-echo -n "$TEXT" | xclip -selection clipboard
+tmpfile=$(mktemp /tmp/dictate-XXXXXX.wav)
+whisper=~/screens/tech/linux_setup/whisper.cpp/
+plan=~/screens/org/Planning/
+sox -d -r 16000 -c 1 "$tmpfile"
+text=$($whisper/build/bin/whisper-cli \
+	-m $whisper/models/ggml-small.en.bin \
+	-f "$tmpfile" --no-timestamps 2>/dev/null)
+echo -n "$text" | xclip -selection clipboard
+echo -n "$text" >> $plan/dump.md
