e6da4ea2122dc8117bae5b0b052959572faee013
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-05-04 17:31:42 -0400

    332

--- scr/dictate.sh
+++ scr/dictate.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 whisper=~/screens/tech/linux_setup/whisper.cpp/
 plan=~/screens/org/Planning/
-segment_secs=4
-
+segment_secs=15
 accumulated=""
 tmpfile=""
 
 screen -S org -p Planning -X stuff $'\e:wall\n'
 
 process_segment() {
-    [ -f "$tmpfile" ] || return
-    local text
-    text=$($whisper/build/bin/whisper-cli \
-        -m $whisper/models/ggml-small.en.bin \
-        -f "$tmpfile" --no-timestamps 2>/dev/null)
-    rm -f "$tmpfile"
-    [ -z "$text" ] && return
-    accumulated="${accumulated}${text} "
-    echo -n "$accumulated" | xclip -selection clipboard
-    printf "%s\n" "$text" >> "$plan/dictate.txt"
+	[ -f "$tmpfile" ] || return
+	local text
+	text=$($whisper/build/bin/whisper-cli \
+		-m $whisper/models/ggml-small.en.bin \
+		-f "$tmpfile" --no-timestamps 2>/dev/null)
+	rm -f "$tmpfile"
+	[ -z "$text" ] && return
+	accumulated="${accumulated}${text} "
+	echo -n "$accumulated" | xclip -selection clipboard
+	printf "%s\n" "$text" >> "$plan/dictate.txt"
+	screen -S org -p Planning -X stuff $'\e:e\n'
+	paplay /usr/share/sounds/freedesktop/stereo/complete.oga ##
 }
 
-cleanup() {
-    pkill -SIGINT sox
-    while pgrep sox > /dev/null; do sleep 0.05; done
-    process_segment
-    exit 0
+record_segment() {
+	tmpfile=$(mktemp /tmp/dictate-XXXXXX.wav)
+	paplay /usr/share/sounds/freedesktop/stereo/bell.oga
+	sox -d -r 16000 -c 1 "$tmpfile" trim 0 $segment_secs &
+	wait $!
+	paplay /usr/share/sounds/freedesktop/stereo/device-removed.oga ##
+	process_segment
 }
-trap cleanup SIGINT SIGTERM
 
-while true; do
-    tmpfile=$(mktemp /tmp/dictate-XXXXXX.wav)
-    paplay /usr/share/sounds/freedesktop/stereo/bell.oga ##
-    sox -d -r 16000 -c 1 "$tmpfile" trim 0 $segment_secs &
-    wait $!
-    process_segment
-    screen -S org -p Planning -X stuff $'\e:e\n'
-    paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-done
+restart() {
+	pkill -SIGINT sox
+	while pgrep sox > /dev/null; do sleep 0.05; done
+	process_segment
+	record_segment
+}
+trap restart SIGINT
+
+record_segment
