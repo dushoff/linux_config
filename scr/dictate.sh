a18a4acf45324eadaeb689b28be7cf76a6aaf397
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-05-03 22:59:29 -0400

    attic

--- scr/dictate.sh
+++ scr/dictate.sh
@@ -6,7 +6,11 @@ sox -d -r 16000 -c 1 "$tmpfile"
 text=$($whisper/build/bin/whisper-cli \
 	-m $whisper/models/ggml-small.en.bin \
 	-f "$tmpfile" --no-timestamps 2>/dev/null)
+
+screen -S org -p Planning -X stuff $'\e:wall\n'
 echo -n "$text" | xclip -selection clipboard
 printf "%s\n" "$text" >> "$plan/dictate.txt"
 
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga ##
+
+screen -S org -p Planning -X stuff $'\e:e\n'
