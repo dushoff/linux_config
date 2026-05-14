8b141ddf83b4744ce4977047eddbb17b885535a6
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-05-14 18:58:38 -0400

    use this one for loopy dictate

--- scr/dictate.sh
+++ scr/dictate.sh
@@ -5,6 +5,7 @@ plan=~/
 segment_secs=20
 tmpfile=""
 text=""
+text=""
 
 ## This is only called from i3 bindings file
 ## It used to actively dump content into the open file in Planning, as well as cache it into dictate and keep the last line in clipboard. Some thought could be applied.
@@ -18,11 +19,16 @@ process_segment() {
 		-m $whisper/models/ggml-small.en.bin \
 		-f "$tmpfile" --no-timestamps 2>/dev/null)
 	rm -f "$tmpfile"
+	text=$(printf '%s' "$text" | tr -d '\n' | sed '
+		s/^[[:space:]]*//
+		s/[[:space:]]*$//m
+		s/([^)]*)//g
+		s/\[[^\]]*\]//g
+		s/[^?.]$/&./
+	')
 }
 
-## Use k (SIGINT) to accumulate and K (SIGHUP
-trap 'interrupted=1; pkill -SIGINT sox' SIGINT
-trap 'pkill -SIGINT sox; process_segment' SIGHUP
+trap 'interrupted=1; pkill -SIGINT sox' SIGHUP
 
 while true; do
 	interrupted=0
@@ -31,13 +37,12 @@ while true; do
 	sox -d -r 16000 -c 1 "$tmpfile" trim 0 $segment_secs &
 	wait $!
 	process_segment
+	## Close if you get to the end without being interrupted
 	[ "$interrupted" -eq 0 ] && break
-	text=$(echo "$text" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//; s/[^?.]$/&./; s/.$/& /')
-	printf "%s\n" "$text" >> "$plan/dictate.txt"
-	## screen -S org -p Planning -X stuff $'\e:e\n'
+	printf "%s " "$text" >> "$plan/dictate.txt"
 	paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 done
 echo -n "$text" | xclip -selection primary
-xclip -o -selection primary
+## xclip -o -selection primary
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
