56eee318e30e697c2e7643a103987d1b87c6a9ef
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-05-24 13:34:52 -0400

    fiVe

--- scr/wsfind.sh
+++ scr/wsfind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ## i3-msg focus parent; focus parent
-i3-msg "[class=\"$1\" workspace=\"__focused__\"] focus" || i3-msg "focus"
+i3-msg "[class=\"$1\" workspace=\"__focused__\"] focus" || i3-msg "[class=\"$1\"] focus"
 i3-msg 'focus child'
