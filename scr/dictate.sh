f00ac03e51b84f2156eb3d3de9a158d69082fc4d
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-05-28 16:47:24 -0400

    Teaching computer…

--- scr/dictate.sh
+++ scr/dictate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 whisper=~/terminal/dirs/setup/whisper.cpp/
-## ~/dictate.txt
-plan=~/
+## ~/dictate.txt ~/dbuff.txt
+store=~/
 segment_secs=12
 tmpfile=""
 fill=""
@@ -42,14 +42,14 @@ while true; do
 	listen
 	process_segment
 	[ "$interrupted" -eq 0 ] && break
-	printf "%s%s" "$text" "$fill" >> "$plan/dictate.txt"
+	printf "%s%s" "$text" "$fill" >> "$store/dictate.txt"
 	printf -v acc "%s%s%s" "$acc" "$text" "$fill"
 	printf "%s" "$acc" | xclip -selection clipboard
 	paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 done
 
 echo -n "$text" | xclip -selection primary
-printf "%s%s" "$text" "$fill" > "~/dictate.txt"
+printf "%s%s" "$text" "$fill" > "$store/dbuff.txt"
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 
