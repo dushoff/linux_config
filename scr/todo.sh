903d67ca33daccd34e798da838ffa96d783d0e00
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-06-07 22:03:20 -0400

    attic

--- scr/todo.sh
+++ scr/todo.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-spin.sh $1.findspace 2>&1 || ${VEDIT} ~/terminal/$1/TODO.md
+# See also scr/basewin.sh under development
+(spin.sh $1.findspace 2>&1 && tmux select-window -t "$1:vim" && tmux send-keys -t "$1:vim" "gj") || ${VEDIT} ~/terminal/$1/TODO.md
