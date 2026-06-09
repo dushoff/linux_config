ae54f86f6704c6748a078c1732e6d9910a6d1ddc
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-06-09 14:29:48 -0400

    332

---
+++ scr/leavetest.pl
@@ -0,0 +1,18 @@
+use strict;
+use 5.10.0;
+
+say "## Logtest";
+
+my $hot = 0;
+
+while(<>){
+	if (/^\t/ or /git pull/ or /rclone/){
+		print;
+		$hot = 1;
+	}
+	if ($hot and /Leaving directory/){
+		$hot=0;
+		print;
+	}
+}
+say "## End logtest";
