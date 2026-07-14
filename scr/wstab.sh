98432124ed3a9b8c8c4d594168d356628cdf202d
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-07-14 17:27:05 -0400

    332

--- scr/wstab.sh
+++ scr/wstab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ## i3-msg focus parent; focus parent
-i3-msg "[class=\"$1\" workspace=\"__focused__\"] focus" || (i3-msg open && i3-msg 'split v; layout tabbed' && exec $2 && i3-msg 'focus child')
+i3-msg "[class=\"$1\" workspace=\"__focused__\"] focus" 2>&1 || (i3-msg open && i3-msg 'split v; layout tabbed' && exec $2 && i3-msg 'focus child')
