e67abe653ef6b49f7ca51c2912caeb2b1c0e01c0
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-08-26 13:59:47 -0400

    332

--- scr/tat.pl
+++ scr/tat.pl
@@ -1,6 +1,4 @@
-## Not remembering why I'm dealing with \n here instead of abstracting
-## Something is confusing, but I should have made notes to myself.
-## Could ask Claude.
+## This is running on file, not lines. Probably for some good reason but it makes the logic a bit confusing.
 s/<br>/\n/g;
 s/&gt;/>/g;
 s/&lt;/</g;
@@ -8,5 +6,8 @@ s/&nbsp;/ /g;
 s/&amp;/&/g;
 s|</?div>||g;
 s|</?span[^>]*>|\n|g;
+
+## s/\n{3,}/\n\n/g;  # safety net: collapse any run of blank lines
+
 s/^[>\s\\]*\n/\n/g;
-s/[>\s\\]*$/\n/g;
+s/[>\s\\]*$/\n/;
