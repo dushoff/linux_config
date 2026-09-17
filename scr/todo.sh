5c4b97e785f3dc29d3e6e690ab333f6bf6c1d44f
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-09-17 13:30:35 -0400

    modified:   todo.sh

--- scr/todo.sh
+++ scr/todo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # See also scr/basewin.sh under development
-# If specified space exists, go there, and select TODO in the corresponding tmux
+# If specified space exists
+## go there, and select TODO in the corresponding tmux
 ## Do not focus the terminal; user can see where they were and then go to terminal
 # else, open a VEDIT of TODO here
 
+## Use focused workspace if no argument provided
 if [ -z "$1" ]; then
 	set -- "$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name' | sed 's/^[0-9]*://')"
 fi
